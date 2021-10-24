abcd0b69ef41d0f3442a72bf65ca61cf11ccd5aa
João Henrique <joaoenrique.san@gmail.com>
2021-10-24 00:50:42 -0300

    Finish implementation

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,6 @@
 let imgSlider = document.querySelector('.img-slider');
 let imgContainer = document.querySelector('.img-container');
+let imgSelector = document.querySelector('.img-selector');
 let arrowRight = document.querySelector('.arrow-right');
 let arrowLeft = document.querySelector('.arrow-left');
 
@@ -10,16 +11,38 @@ arrowLeft.addEventListener('click', previousImage);
 let images = [];
 let actualImgIndex = 0;
 
-for(let image of imgContainer.children){
-    images.push(image);
+for (let index = 0; index < imgContainer.children.length; index++) {
+    images.push(imgContainer.children[index]);
+    let newEL = document.createElement('p');
+    newEL.innerHTML = index;
+    newEL.addEventListener('click', changeImg);
+    imgSelector.appendChild(newEL);
 }
+updateSelector();
+
 function nextImage(e){
     actualImgIndex < images.length-1 ? actualImgIndex++ : actualImgIndex = 0;
     let actualMargin = imgContainer.style.marginLeft;
     imgContainer.style.marginLeft = `${-500 * actualImgIndex}px`;
+    updateSelector();
 }
 function previousImage(e){
     actualImgIndex > 0 ? actualImgIndex-- : actualImgIndex = images.length -1;
     let actualMargin = imgContainer.style.marginLeft;
     imgContainer.style.marginLeft = `${-500 * actualImgIndex}px`;
+    updateSelector();
 }
+function changeImg(e){
+    actualImgIndex = e.target.innerHTML;
+    let actualMargin = imgContainer.style.marginLeft;
+    imgContainer.style.marginLeft = `${-500 * actualImgIndex}px`;
+    updateSelector();
+}
+function updateSelector(){
+    for(let selector of imgSelector.children){
+        selector.classList.remove('selected');
+    }
+    imgSelector.children[actualImgIndex].classList.add('selected');
+}
+
+setTimeout(nextImage, 5000);
